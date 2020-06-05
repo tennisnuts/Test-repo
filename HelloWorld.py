44fe77d31346bc44cc590cf235c40e0fee592ba5
Oliver Nunn <oliver.nunn@yahoo.co.uk>
2020-06-05 07:22:36 +0100

    Removed some Code

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,19 +1,13 @@
-import math
-import os
-import sys
 
-import numpy
 import requests
 
-# print(sys.version)
-# print(sys.executable)
 
 
-# def greet(who_to_greet):
-#     greeting = "Hello, {}".format(who_to_greet)
-#     return greeting
+
+q = 76
 
 
 r = requests.get("https://google.com")
 print(r.status_code)
 
+print(quit)
